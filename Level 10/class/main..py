8268860a43228b001c8cd7202dff15fa8f18430b
giorgigomareli7 <giorgigomareli7@gmail.com>
2026-05-13 17:22:06 +0400

    10th commit

---
+++ Level 10/class/main..py
@@ -0,0 +1,5 @@
+values = [10, 5.7, 'hello', True, 99.9, 'python', False, 45, 3.14, 'world']
+
+print(values[2:6])
+print(values[1:4])
+print(values[-5:-1])
---
+++ Level 10/home/home.py
@@ -0,0 +1,13 @@
+numbers = [2,6,8,12,44,62,56,99]
+print(numbers[:4])
+
+students = ['giorgi','irakli','saba','ana','mari','luka']
+print(students[-3:])
+
+fruits = ['orange','apple','banana','grapes','watermelon','mango','pineapple']
+print(fruits[2:6])
+
+country = ['georgia','spain','france','germany','japan','usa','belgium','poland']
+print(country[:-1])
+
+
